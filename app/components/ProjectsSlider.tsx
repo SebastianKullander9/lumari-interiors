109ec3170be22e98bf4b53a254cfacf891f1f6ba
Sebastian Kullander <sebastian.kullander@protonmail.com>
2025-08-20 13:34:08 +0200

    add view to display projects in a carousel

---
+++ app/components/ProjectsSlider.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React, { useState, useCallback } from "react";
+import { EmblaOptionsType } from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
+
+type PropType = {
+    slides: number[]
+    options?: EmblaOptionsType
+}
+
+const slideContent = [
+    { url: "/projects/project-s.jpeg", name: "PROJECT S", text: "An apartment in the heart of Stockholm, featuring a variety of custom designs. We blended the apartment’s historic charm with modern elements to create a harmonious and stylish living space." },
+	{ url: "/projects/project-lili.jpeg", name: "PROJECT LILI", text: "A luxury villa built entirely from scratch, inspired by the warm and inviting aesthetics of Southern Europe, located in the stunning Swedish archipelago." },
+    { url: "/projects/project-r.jpeg", name: "PROJECT R", text: "A real estate office designed with premium furnishings and a style that reflects the company’s unique personality and commitment to quality." },
+]
+
+export default function ProjectsSlider() {
+    const SLIDE_COUNT = slideContent.length;
+	const slides = Array.from(Array(SLIDE_COUNT).keys());
+    const options = { loop: true }
+    const [emblaRef] = useEmblaCarousel(options);
+
+    return(
+        <section className="w-screen h-screen">
+            <div className="overflow-hidden h-full" ref={emblaRef}>
+                <div className="flex touch-pan-y ml-[-1rem] h-full items-center">
+                    {slides.map((index) => (
+                        <div
+                            key={index}
+                            className="flex-[0_0_50%] min-w-0 pl-4 mr-12"
+                        >
+                            <div className="relative h-[65vh] flex items-center justify-center select-none hover:scale-103 transition-all duration-300 cursor-pointer">
+                                <Image
+                                src={slideContent[index].url}
+                                fill
+                                alt={slideContent[index].name}
+                                className="object-cover"
+                                />
+                            </div>
+                            <div className="pt-4">
+                                <h1 className="text-lg">{slideContent[index].name}</h1>
+                                <p className="text-sm text-gray-600">{slideContent[index].text}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import HomeView from "./components/HomeView";
 import Header from "./components/Header";
+import ProjectsSlider from "./components/ProjectsSlider";
 
 export default function Home() {
     return (
         <div>
 			<Header />
             <HomeView />
-			<div className="test h-screen w-screen"></div>
+			<ProjectsSlider />
         </div>
     );
 }
