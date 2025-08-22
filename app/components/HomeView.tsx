f3681d530482b77d158c84a94674de6b4e450953
Sebastian Kullander <sebastian.kullander@protonmail.com>
2025-08-22 15:26:27 +0200

    fixed image background, revert style on welcome page

--- app/components/HomeView.tsx
+++ app/components/HomeView.tsx
@@ -1,16 +1,16 @@
 export default function HomeView() {
-    return(
+    return (
         <section>
-            <div style={{ backgroundImage: "url('/home-background.jpg')" }}
-                 className="w-screen h-screen bg-cover bg-[center_70%] relative"
+            <div
+                style={{ backgroundImage: "url('/home-background.jpg')" }}
+                className="w-screen h-screen bg-cover bg-center bg-fixed relative"
             >
                 <div className="absolute w-screen h-screen flex justify-center items-center">
-                    <div className="flex flex-col justify-center items-center sm:block sm:w-[60vw] md:w-[80vw] lg:w-[60vw]">
-                        <h1 className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl text-white tracking-widest font-semibold">WE CREATE</h1>
-                        <h1 className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl text-white tracking-widest font-semibold flex justify-end">PERSONALITY</h1>
-                    </div>
+                    <h1 className="text-xl md:text-4xl lg:text-7xl text-white tracking-widest font-semibold">
+                        WE CREATE PERSONALITY
+                    </h1>
                 </div>
             </div>
         </section>
-)
+    );
 }
